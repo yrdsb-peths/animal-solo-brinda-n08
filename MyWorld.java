225d3bec03ec67cdaad33af8cf761266df77a182
brinda-n08 <349035105@gapps.yrdsb.ca>
2025-04-29 09:22:37 -0400

    Added Apple

--- MyWorld.java
+++ MyWorld.java
@@ -13,6 +13,17 @@ public class MyWorld extends World {
         Elephant elephant = new Elephant();
         addObject(elephant,300,200);
         
-       
+        createApple();
+    }
+    
+    /**
+     * Create a new apple at random location at top of screen
+     */
+    public void createApple()
+    {
+        Apple apple = new Apple();
+        int x = Greenfoot.getRandomNumber(600);
+        int y = 0;
+        addObject(apple, x, y);
     }
 }
