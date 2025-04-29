199dd7ab93c0123f5a61bd8512e416ca2b260fc3
brinda-n08 <349035105@gapps.yrdsb.ca>
2025-04-29 09:00:42 -0400

    Added Elephant

--- MyWorld.java
+++ MyWorld.java
@@ -1,7 +1,18 @@
 import greenfoot.*;
 
+/**
+ * The World our hero lives in.
+ * 
+ * @author Brinda
+ * @version April 2025
+ */
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        
+        Elephant elephant = new Elephant();
+        addObject(elephant,300,200);
+        
+       
     }
 }
